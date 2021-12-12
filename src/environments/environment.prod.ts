ac8061dffc741f1bacdf300de93aceb236f4d67a
lbk2knewlifegithub <lbk2knewlife@gmail.com>
2021-12-12 18:17:36 +0700

    Update apikey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   api: 'https://geo.ipify.org/api/v2/country,city',
-  apiKey: 'at_tVAISQQy0u9ufASO6nHu912oafq0i'
+  apiKey: 'at_wF3neWnwcVQIU3kjbLvkcV5uURUo7'
 };
